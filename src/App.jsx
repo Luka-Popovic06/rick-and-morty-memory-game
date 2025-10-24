ed7fbefe714bf3436b0bd8ceb577052570506f9b
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-24 12:54:52 +0200

    Add global CSS reset

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,9 @@ import { rickAndMortyCaracters } from "./caracters.js";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [caracters, setCaracters] = useState(rickAndMortyCaracters);
 
-  return (
-    <>
-      <header className="header">
-        <h1 className="title">Rick and Morty memory game</h1>
-      </header>
-      <main className="container"></main>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
