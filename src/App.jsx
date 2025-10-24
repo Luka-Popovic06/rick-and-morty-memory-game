b471f8fca1b4944cf622f1d0bc6deeb236147b1f
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-24 12:47:17 +0200

    Add Rick and Morty characters data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { rickAndMortyCaracters } from "./caracters.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <header className="header">
+        <h1 className="title">Rick and Morty memory game</h1>
+      </header>
+      <main className="container"></main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/caracters.js
@@ -0,0 +1,74 @@
+export const rickAndMortyCaracters = [
+  {
+    img: "snuffles.120ea6e983a68aef7be5.png",
+    firstName: "Snuffles",
+    lastName: "",
+    id: 1,
+  },
+  {
+    img: "rickkk.c06ec7790deb77a3746b.png",
+    firstName: "Rick",
+    lastName: "Sanchez",
+    id: 2,
+  },
+  {
+    img: "J19ick.13cdd5d095ba92ee1ce4.png",
+    firstName: "Doofus",
+    lastName: "Rick",
+    id: 3,
+  },
+  {
+    img: "birdPerson.bbeb4909b0b4a608592c.png",
+    firstName: "Bird",
+    lastName: "Person",
+    id: 4,
+  },
+  {
+    img: "michael.8c9961c6a42e6577b366.png",
+    firstName: "Krombopulos",
+    lastName: "Michael",
+    id: 5,
+  },
+  {
+    img: "vampire.4c634082ab10e93076ac.png",
+    firstName: "Coach",
+    lastName: "Feratu",
+    id: 6,
+  },
+  {
+    img: "president.d8927b18ea37085c4685.png",
+    firstName: "President",
+    lastName: "Curtis",
+    id: 7,
+  },
+  {
+    img: "abraham.5bab582d158024aecc0a.png",
+    firstName: "Abrodolph",
+    lastName: "Lincoler",
+    id: 8,
+  },
+  {
+    img: "squanchy.a39194b73a966ab61d06.png",
+    firstName: "Squanchy",
+    lastName: "",
+    id: 9,
+  },
+  {
+    img: "princeNebulon.492189f9013fb3a2c907.png",
+    firstName: "Prince",
+    lastName: "Nebulon",
+    id: 10,
+  },
+  {
+    img: "Terry.0005c5c221a69a614d0a.png",
+    firstName: "Scarry",
+    lastName: "Terry",
+    id: 11,
+  },
+  {
+    img: "gearHead.0eff37e1f38fe1f04e25.png",
+    firstName: "Gear",
+    lastName: "Head",
+    id: 12,
+  },
+];
