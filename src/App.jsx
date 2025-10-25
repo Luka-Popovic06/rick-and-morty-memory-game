8c201bc841aebbd60790c7bff234b6587f881202
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-25 10:53:39 +0200

    set up App component structure with state for characters, selected card, and score

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,11 @@ import { useState, useEffect } from "react";
 import { rickAndMortyCharacters } from "./caracters.js";
 import Caracter from "./Caracter.jsx";
 import "./App.css";
+
+function App() {
+  const [characters, setCharacters] = useState(rickAndMortyCharacters);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [score, setScore] = useState({ current: 0, high: 0 });
+}
+
+export default App;
