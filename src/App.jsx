fa3535c473959d194462bfffa8dc42e8b2babc77
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-25 10:42:06 +0200

    Add game over overlay and play again button styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import { useState, useEffect } from "react";
-import { rickAndMortyCaracters } from "./caracters.js";
+import { rickAndMortyCharacters } from "./caracters.js";
 import Caracter from "./Caracter.jsx";
 import "./App.css";
-
-function App() {
-  const [caracters, setCaracters] = useState(rickAndMortyCaracters);
-
-  return <></>;
-}
-
-export default App;
