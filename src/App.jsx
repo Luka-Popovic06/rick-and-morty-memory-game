6c6fd5f36925e02f41dfe5a51b4b50b341f1291a
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-25 10:55:11 +0200

    implement shuffle, character selection, and reset logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,40 @@ function App() {
   const [characters, setCharacters] = useState(rickAndMortyCharacters);
   const [selectedCard, setSelectedCard] = useState({});
   const [score, setScore] = useState({ current: 0, high: 0 });
+
+  const shuffle = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const random = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[random]] = [newArray[random], newArray[i]];
+    }
+    return newArray;
+  };
+  const markCharacterAsClicked = (id) => {
+    setCharacters((prev) =>
+      prev.map((character) =>
+        character.id === id ? { ...character, isClicked: true } : character
+      )
+    );
+  };
+  const selectCharacter = (id) => {
+    const selectedObject = characters.find((character) => character.id === id);
+    setSelectedCard(selectedObject);
+  };
+  const reset = () => {
+    setScore((prev) => ({
+      current: 0,
+      high: prev.high <= prev.current ? prev.current : prev.high,
+    }));
+    setSelectedCard({});
+    characters.map((character) => {
+      character.isClicked = false;
+    });
+  };
+  useEffect(() => {
+    const newShuffled = shuffle(characters);
+    setCharacters(newShuffled);
+  }, [score.current]);
 }
 
 export default App;
