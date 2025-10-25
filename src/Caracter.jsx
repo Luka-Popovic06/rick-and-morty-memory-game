aaf25a500cf9b7f93a7eabfeab9479fcbc1c88e0
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-25 10:43:21 +0200

    Update Caracter component with click logic and score handling

--- src/Caracter.jsx
+++ src/Caracter.jsx
@@ -1,1 +1,29 @@
 import React from "react";
+const Caracter = (props) => {
+  const {
+    id,
+    img,
+    firstName,
+    lastName,
+    clicked,
+    setScore,
+    selectCard,
+    isClicked,
+  } = props;
+  return (
+    <li
+      className="list-item"
+      onClick={() => {
+        !isClicked && setScore();
+        clicked(id);
+        selectCard(id);
+      }}
+    >
+      <img className="caracter-img" src={img} alt={`${firstName}-img`} />
+      <p className="name">
+        {firstName} {lastName}
+      </p>
+    </li>
+  );
+};
+export default Caracter;
